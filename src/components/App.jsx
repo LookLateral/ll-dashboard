86aee51011e47eed60026efd56ae72dfedfa5f0b
Simon <simone.baroni@looklateral.com>
2019-01-22 17:43:33 +0100

    WIP v0.3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,36 +4,23 @@ import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
 import './App.css';
 import aws_exports from '../aws-exports';
-import Amplify, { Auth, API } from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 
 Amplify.configure(aws_exports);
 
 class App extends React.Component {
-  constructor(props/*, context*/) {
-    super(props/*, context*/);
+  constructor(props) {
+    super(props);
     this.state = {
-      registered:false,
-      approved:false,
-      step1:false,
-      step2:false,
       email:'',
       firstName:'',
       middleName:'',
       surname:'',
-      address:'',
-      city:'',
-      zipCode:'',
-      regionState:'',
-      occupation:'',
-      countryCitizenship:'',
-      countryResidence:'',
-      dateBirth:'',
-      accreditedInvestor: false,
-      amount:'',
+      token:0,
     }
   }
 
-  getUser = async () => {
+  /*getUser = async () => {
     const response = await API.get('preKYCapi', '/items/object/' + this.state.email);
     //if(response) console.log (JSON.stringify(response));
 
@@ -51,14 +38,7 @@ class App extends React.Component {
       });
     }
 
-    /*if(response.amount && response.amount !== this.state.amount) { //enough to say step2 done
-      this.setState({    // we don't need to set all the state!!      
-        step2:true,
-        accreditedInvestor:response.accreditedInvestor,
-        amount:response.amount,
-      });
-    }*/
-  }
+  }*/
   
 
   render() {
